3fc98097dce1e0b503a05827e9ff5aa9710166b1
Leon1772 <55985353+Leon1772@users.noreply.github.com>
2020-02-29 13:26:24 +0000

    changed models

--- app/models.py
+++ app/models.py
@@ -1,12 +1,12 @@
-from flask_login import UserMixin
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask_bcrypt import generate_password_hash, check_password_hash
 
-from app import db
+from langbridge import db
+from datetime import datetime
 
 
-class User(UserMixin, db.Model):
+class User(db.Model):
     __tablename__ = "user"
-    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
     email = db.Column(db.Text, unique=True)
     password = db.Column(db.Text)
@@ -27,46 +27,62 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password, password)
 
 
-class Student(User):
-    __tablename__ = 'student'
-    id = db.Column(None, db.ForeignKey('user.id'), primary_key=True)
-    student_ref = db.Column(db.Integer)
-    grades = db.relationship('Grade', backref='students')
-
-    __mapper_args__ = {"polymorphic_identity": "student"}
-
-    def __repr__(self):
-        return '<Student ID: {}, name: {}>'.format(self.student_ref, self.name)
-
-
 class Teacher(User):
     __tablename__ = 'teacher'
-    id = db.Column(None, db.ForeignKey('user.id'), primary_key=True)
-    teacher_ref = db.Column(db.Text, nullable=False)
-    title = db.Column(db.Text)
-    courses = db.relationship('Course', backref='teachers')
+    teacher_id = db.Column(None, db.ForeignKey('user.user_id'), primary_key=True)
+    rating = db.Column(db.Integer, nullable=False)
+    reviews = db.Column(db.String)
 
-    __mapper_args__ = {"polymorphic_identity": "teacher"}
-
-    def __repr__(self):
-        return '<Teacher {} {}>'.format(self.teacher_ref, self.title, self.name)
-
-
-class Course(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    course_code = db.Column(db.Integer, nullable=False)
-    name = db.Column(db.Text, nullable=False)
-    teacher_id = db.Column(db.Integer, db.ForeignKey(Teacher.id), nullable=False)
-    grades = db.relationship('Grade', backref='courses')
+    __mapper_args__ = {"polymorphic_identity": "learner"}
 
     def __repr__(self):
-        return '<Course {}>'.format(self.code, self.name)
+        return f"User('{self.username}', '{self.email}')"
 
 
-class Grade(db.Model):
-    student_id = db.Column(db.Integer, db.ForeignKey(Student.id), nullable=False, primary_key=True)
-    course_id = db.Column(db.Integer, db.ForeignKey(Course.id), nullable=False, primary_key=True)
-    grade = db.Column(db.Text)
+class BankAccount(db.Model):
+    __tablename__ = 'bank_account'
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), primary_key=True)
+    payment_type = db.Column(db.String(250), nullable=False)
+    payment_details = db.Column(db.String(250), nullable=False)
+
 
     def __repr__(self):
-        return '<Grade {}>'.format(self.grade)
+        return f"Forecast('{self.forecast}', '{self.comment}')"
+
+
+class Wallet(db.Model):
+    __tablename__ = 'wallet'
+    wallet_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    balance = db.Column(db.Integer)
+
+
+class Lesson(db.Model):
+    __tablename__ = 'lesson'
+    lesson_id = db.Column(db.Integer, primary_key=True)
+    learner_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    teacher_id = db.Column(db.Integer, db.ForeignKey('teacher.teacher_id'))
+    aim = db.Column(db.String)
+    format = db.Column(db.String)
+    time = db.Column(db.String)
+
+
+class LessonReview(db.Model):
+    __tablename__ = 'lesson_review'
+    review_id = db.Column(db.Integer, primary_key=True)
+    learner_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    rating = db.Column(db.Integer)
+    comment = db.Column(db.String)
+    reviewed_on = db.Column(db.String)
+
+
+class Language(db.Model):
+    __tablename__ = 'language'
+    lang_id = db.Column(db.Integer, primary_key=True)
+    language = db.Column(db.String)
+
+
+class LanguageUser(db.Model):
+    __tablename__ = 'language_user'
+    lang_id = db.Column(db.Integer, db.ForeignKey('language.lang_id'), primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), primary_key=True)
