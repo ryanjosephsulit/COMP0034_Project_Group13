e7e48f1da4b6ff8f75a1aaff8ed9071021f1489c
Ryan Joseph Sulit Velasquez <zcecrjs@ucl.ac.uk>
2020-03-07 20:46:53 +0000

    Creation of new branch
    Editing base functionality of base.html (into base2.html), as well as index.html

--- langbridge/main/routes.py
+++ langbridge/main/routes.py
@@ -3,30 +3,50 @@ from datetime import datetime
 from flask import render_template, Blueprint, request, flash, redirect, url_for, make_response
 from sqlalchemy import or_
 from sqlalchemy.orm import with_polymorphic
-
+from langbridge.auth.forms import SignupForm, LoginForm
 from langbridge import db
 from langbridge.models import Teacher, User, BankAccount, Wallet, Language, Lesson, LessonReview
-
 
 bp_main = Blueprint('main', __name__)
 
 
-@bp_main.route('/')
+@bp_main.route('/', methods=['POST', 'GET'])
 def index(name=""):
     if 'name' in request.cookies:
         name = request.cookies.get('name')
-    return render_template('index.html', name=name)
+    form = SignupForm(request.form)
+    if request.method == 'POST' and form.validate():
+        if form.role.data == "learner":
+            user = User(name=form.name.data, email=form.email.data, lang_id=form.language.data)
+        else:
+            user = Teacher(name=form.name.data, title=form.title.data, email=form.email.data,
+                           lang_id=form.language.data)
+        user.set_password(form.password.data)
+        try:
+            db.session.add(user)
+            db.session.commit()
+            response = make_response(redirect(url_for('main.index')))
+            response.set_cookie("name", form.name.data)
+            return response
+        except IntegrityError:
+            db.session.rollback()
+            flash('ERROR! Unable to register {}. Please check your details are correct and resubmit'.format(
+                form.email.data), 'error')
+
+    return render_template('index.html', name=name, form=form)
 
 
 @bp_main.route('/languages', methods=['GET'])
 def language():
     languages = Language.query.join(Teacher).with_entities(Language.lang_id, Language.name,
-                                                       Teacher.name.label('user_name'), Teacher.email).order_by(Language.lang_id).all()
+                                                           Teacher.name.label('user_name'), Teacher.email).order_by(
+        Language.lang_id).all()
     print("HERE")
     # Simple test to see if languages is populated
     print(languages)
 
     return render_template("languages.html", language=languages)
+
 
 @bp_main.route('/delete_cookie')
 def delete_cookie():
